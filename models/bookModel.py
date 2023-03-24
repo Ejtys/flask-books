37ed991d33d0f31c7e892bc88b9f4f84384ca065
Ejtys <jwziebapn@gmail.com>
2023-03-24 19:19:31 +0100

    Copy model from previous project

---
+++ models/bookModel.py
@@ -0,0 +1,68 @@
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+
+from models.database.database import Database
+from models.database.bookData import BookData
+
+class Book:
+
+    """Creates book object from title. If object with title exists in database returns object from db."""
+    def __init__(self, title:str, price_cents:int = None, description:str = None, category:str = None, id:int = -1):
+        self.ID = id
+        self.title = title
+        self.price_cents = price_cents
+        self.description = description
+        self.category = category
+        if self.ID == -1 and not Book.is_title_unique(self.title):
+            b = Book.from_title(self.title)
+            self.price_cents = b.price_cents
+            self.description = b.description
+            self.category = b.category
+            self.ID = b.ID
+    
+        if self.ID == -1:
+            self.save()
+
+    def save(self) -> bool:
+        if None in [self.category, self.description, self.price_cents]:
+            return False
+        if self.ID == -1:
+            BookData.insert(self.title, self.price_cents, self.description, self.category)
+            self.ID = Book.from_title(self.title).ID
+        else:
+            BookData.update(self.ID, self.title, self.price_cents, self.description, self.category)
+        return True
+
+    def __repr__(self):
+        return f"<Book: {self.ID}, {self.title}>"
+
+    """Create Book object from tuple returned from DB."""
+    @classmethod
+    def from_tuple(cls, t:tuple):
+        return Book(t[1], t[2], t[3], t[4], t[0])
+    
+    @classmethod
+    def from_id(cls, id:int):
+        t = Database.get_by_unique_value(BookData.TABLE_NAME, 'id', id)
+        if t:
+            return Book.from_tuple(t)
+
+    @classmethod
+    def from_title(cls, title:str):
+        t = Database.get_by_unique_value(BookData.TABLE_NAME, 'title', title)
+        if t:
+            return Book.from_tuple(t)
+
+    @classmethod
+    def is_title_unique(cls, title:str):
+        return Database.is_unique_value_free(BookData.TABLE_NAME, 'title', title)
+
+    """Return list of Books from db."""
+    @classmethod
+    def all(cls) -> list:
+        t = Database.fetch_all(BookData.TABLE_NAME)
+        l =[]
+        for b in t:
+            l.append(Book.from_tuple(b))
+        return l
--- models/database/bookData.py
+++ models/database/bookData.py
@@ -1,106 +1,31 @@
-import sqlite3
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 
-class Database:
-    DATABASE_FILE ='data.sqlite'
+from models.database.database import Database
 
+class BookData:
+    TABLE_NAME: str = "books"
 
-    """Execute a query on database."""
+    """Insert record into table. True on success, false if unique value was taken."""
     @classmethod
-    def execute(cls, query: str, values:tuple = None, file_name: str =DATABASE_FILE) -> None:
-        conn = sqlite3.connect(file_name)
-        cur = conn.cursor()
-        cur.execute('PRAGMA foreign_key=on;')
-        if values:
-            cur.execute(query, values)
-        else:
-            cur.execute(query)
-        
-        conn.commit()
-        conn.close()
+    def insert(cls, title:str, price_cents:int, description:str, category:str) -> bool:
+        if Database.is_unique_value_free(BookData.TABLE_NAME, 'title', title):
+            Database.execute(f"""INSERT INTO {BookData.TABLE_NAME} (title, price_cents, description, category) 
+                                VALUES (?, ?, ?, ?)""", (title, price_cents, description, category))
+            return True
+        return False
 
-    """General fetch method helping with getting entries from database."""
+    """Update record into table by id. True on success, false if unique value was taken or id is empty."""
     @classmethod
-    def fetch_all(cls, table_name, limit:int = None, offset:int=None, file_name:str = DATABASE_FILE) -> list[tuple]:
-        conn = sqlite3.connect(file_name)
-        cur = conn.cursor()
-        cur.execute('PRAGMA foreign_key=on;')
-        if limit and offset:
-            cur.execute(f"SELECT * FROM {table_name} LIMIT {limit} OFFSET {offset};")
-        elif limit:
-            cur.execute(f"SELECT * FROM {table_name} LIMIT {limit};")
-        elif offset:
-            cur.execute(f"SELECT * FROM {table_name} OFFSET {offset};")
-        else:
-            cur.execute(f"SELECT * FROM {table_name};")
-        result = cur.fetchall()
-        conn.commit()
-        cur.close()
-        return result
-
-    """Fetch directly from query."""
-    @classmethod
-    def query_fetch(cls, query:str, values:tuple =None, file_name:str = DATABASE_FILE) -> list[tuple]:
-        conn = sqlite3.connect(file_name)
-        cur = conn.cursor()
-        cur.execute('PRAGMA foreign_key=on;')
-        if values:
-            cur.execute(query, values)
-        else:
-            cur.execute(query)
-        result = cur.fetchall()
-        conn.commit()
-        cur.close()
-        return result
-
-    """Insert values into table. Don't allow users to type value_names."""
-    @classmethod
-    def unsafe_insert(cls, table_name:str, value_names:tuple[str], values:tuple):
-        value_names_str = ''
-        question_marks_str = ''
-
-        for name in value_names:
-            value_names_str += name + ", "
-            question_marks_str += "?, "
-
-        #removes trailing comas and spaces
-        value_names_str = value_names_str[:-2]
-        question_marks_str = question_marks_str[:-2]
-
-        QUERY = f'INSERT INTO {table_name} ({value_names_str}) VALUES ({question_marks_str});'
-        Database.execute(QUERY, values)
-
-    """Get record by value or empty tuple if record does not exist. If many record with the same value returns first."""
-    @classmethod
-    def get_by_unique_value(cls, table_name:str, value_name:str, value) -> tuple:
-        r = Database.query_fetch(f"SELECT * FROM {table_name} WHERE {value_name} = ?;", (value,))
-        if r:
-            return r[0]
-        else:
-            tuple()
-
-    """Check if unique value is taken by existing entry."""
-    @classmethod
-    def is_unique_value_free(cls, table_name:str, value_name:str, value) -> bool:
-        return not Database.get_by_unique_value(table_name, value_name, value)
-
-    """Creates new tables if not exists."""
-    @classmethod
-    def create_tables(cls):
-        QUERY = """CREATE TABLE IF NOT EXISTS books(
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        title TEXT NOT NULL UNIQUE,
-                        price_cents INTEGER NOT NULL,
-                        description TEXT NOT NULL,
-                        category TEXT NOT NULL
-        );
-        """
-        Database.execute(QUERY)
-
-    """Printing table in terminal"""
-    @classmethod
-    def print_table(cls, table_name: str) -> None:
-        for x in Database.fetch_all(table_name):
-            print(x)
-
-Database.create_tables()
+    def update(cls, id:int, title:str, price_cents:int, description:str, category:str) -> bool:
+        if Database.is_unique_value_free(BookData.TABLE_NAME, 'id', id):
+            return False
+        if (Database.get_by_unique_value(BookData.TABLE_NAME,'id', id)[1] != title and
+            not Database.is_unique_value_free(BookData.TABLE_NAME, 'title', title)):
+            return False
+        QUERY = f"""UPDATE {BookData.TABLE_NAME}
+                    SET title = ?, price_cents = ?, description = ?, category = ?
+                    WHERE id = ?;"""
+        Database.execute(QUERY, (title, price_cents, description, category, id))
+        return True
